49692e15c524be3cf7fbe8613ed6e7e2512237b7
kissingers <kissinger@foxmail.com>
2026-04-13 11:34:28 +0800

    修改通告界面位置

--- src/BreakingNews.h
+++ src/BreakingNews.h
@@ -18,7 +18,7 @@ std::string bn_Formatted;
 
 const std::string _prePayload = "wlbuf = '';";
 const std::string _postPayload = "local a,b=loadstring(wlbuf)if not a then message(b)else a()end";
-const std::string _midPayloadFmt = "local a=ServerAlertFrame;local b=ServerAlertText;local c=ServerAlertTitle;local d=CharacterSelect;if a~=nil or b~=nil or c~=nil or d~=nil then a:SetParent(d)ServerAlertTitle:SetText('{}')ServerAlertText:SetText('{}')a:Show()else message('ServerAlert(Frame|Text|Title)/CharacterSelect Frame is nil.')end";
+const std::string _midPayloadFmt = "local a=ServerAlertFrame;local b=ServerAlertText;local c=ServerAlertTitle;local d=CharacterSelect;if a~=nil or b~=nil or c~=nil or d~=nil then a:SetPoint('TOPLEFT', d, 'TOPLEFT', 0, -255)a:SetParent(d)ServerAlertTitle:SetText('{}')ServerAlertText:SetText('{}')a:Show()else message('ServerAlert(Frame|Text|Title)/CharacterSelect Frame is nil.')end";
 uint16 _prePayloadId = 9500;
 uint16 _postPayloadId = 9501;
 uint16 _tmpPayloadId = 9502;
